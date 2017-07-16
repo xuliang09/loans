7f5fd2210e8834ab34a510b30b29c22d1522afb7
xul <xuliang09@foxmail.com>
2017-07-16 20:41:12 +0800

    initial commit

---
+++ src/main/java/com/loans/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.loans.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 用户界面交互类
+ * 〈一句话功能简述〉
+ *
+ * <p>〈功能详细描述〉
+ * @author xul 2017年7月16日
+ * @see [相关类/方法]
+ * @since 1.0
+ */
+@Controller
+public class MainController
+{
+    // 判断连通
+    @RequestMapping(value = "/isalive", method = RequestMethod.GET)
+    public @ResponseBody String isAlive()
+    {
+        return "alive!";
+    }
+}
