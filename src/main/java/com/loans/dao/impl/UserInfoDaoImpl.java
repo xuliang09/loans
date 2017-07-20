aae1698db84fa9174e92d5d89ea84a8ccc6910df
xul <xuliang09@foxmail.com>
2017-07-20 22:51:58 +0800

    规范了dao和service的接口

--- src/main/java/com/loans/dao/impl/UserInfoDaoImpl.java
+++ src/main/java/com/loans/dao/impl/UserInfoDaoImpl.java
@@ -1,15 +1,37 @@
 package com.loans.dao.impl;
 
 import com.loans.dao.UserInfoDao;
-import com.loans.domain.User;
+import com.loans.domain.CompanyOrIndividual;
+import com.loans.domain.OfficeWorker;
 
 public class UserInfoDaoImpl implements UserInfoDao
-{    
+{
     @Override
-    public int addUser(User user)
+    public int addCompanyOrIndividual(CompanyOrIndividual user)
     {
-        // TODO Auto-generated method stub
-        return 0;
+        try
+        {
+            return 0;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return 1;
+        }
     }
-    
+
+    @Override
+    public int addOfficeWorker(OfficeWorker user)
+    {
+        try
+        {
+            return 0;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
 }
