39ba1ecc669f5e7627c89e365f11aa92aaed665a
xul <294739212@qq.com>
2017-09-16 19:19:02 +0800

    fix some bugs

--- src/main/java/com/loans/sms/CloudMessage.java
+++ src/main/java/com/loans/sms/CloudMessage.java
@@ -107,8 +107,8 @@ public class CloudMessage
 
         request.setTemplateCode("SMS_79685046");
         // 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-        request.setTemplateParam("{\"name\":" + name + ", \"phoneNumber\":"
-                + phoneNumber1 + "}");
+        request.setTemplateParam("{\"name\":\"" + name + "\", \"phoneNumber\":\""
+                + phoneNumber1 + "\"}");
 
         // 选填-上行短信扩展码(无特殊需求用户请忽略此字段)
         // request.setSmsUpExtendCode("90997");
